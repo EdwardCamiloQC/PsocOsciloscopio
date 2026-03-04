0cb163437f23a6993879955e241ac04f68121f24
EdwardCamiloQC <edward.camilo.q@gmail.com>
2026-03-04 15:46:08 -0500

    Update the Topdesign using ADC_SAR_Seq.

--- Osc.cydsn/main.c
+++ Osc.cydsn/main.c
@@ -2,91 +2,82 @@
 #include <stdlib.h>
 
 //===============================================
-//========== MACROS
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// MACROS
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //===============================================
 #define CHANNELS              4
 #define BYTES_PER_SAMPLE      (CHANNELS * 2)
-#define RING_SIZE             4096
-#define USB_PACKET_SIZE       64
+
+#define USB_PACKET_SIZE       32
 
 #define DMA_BYTES_PER_BURST   2
 #define DMA_REQUEST_PER_BURST 1
 
 //===============================================
-//========== VARIABLES GLOBALES
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// VARIABLES GLOBALES
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //===============================================
-volatile uint8 ringBuffer[RING_SIZE];
-volatile uint16 writeIndex = 0;
-
-uint8 usbPacket[USB_PACKET_SIZE];
+uint16 usbPacketSignals[USB_PACKET_SIZE/2];
+volatile uint8 flagComplete = 0;
 
 uint8 dmaChannel;
 uint8 dmaTd[1];
 
-volatile uint8 muxChannel = 0;
+uint8 prueba[USB_PACKET_SIZE];
 
 //===============================================
-//========== ISRs
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ISRs
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //===============================================
 CY_ISR(DMA_ISR){
     CyDmaClearPendingDrq(dmaChannel);
-    writeIndex += BYTES_PER_SAMPLE;
-    if(writeIndex >= RING_SIZE){
-        writeIndex = 0;
-    }
-    muxChannel++;
-    if(muxChannel >= 4){
-        muxChannel = 0;
-    }
-    AMux_1_Select(muxChannel);
+    flagComplete = 1;
 }
 
 
 //===============================================
-//========== FUNCTIONS
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// FUNCTIONS
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //===============================================
 void DMA_Config(void){
     dmaChannel = DMA_1_DmaInitialize(DMA_BYTES_PER_BURST, DMA_REQUEST_PER_BURST, HI16(CYDEV_PERIPH_BASE), HI16(CYDEV_SRAM_BASE));
     
     dmaTd[0] = CyDmaTdAllocate(); //DMA transfer descriptor
     
-    CyDmaTdSetConfiguration(dmaTd[0], RING_SIZE, dmaTd[0], TD_INC_DST_ADR | TD_AUTO_EXEC_NEXT);
-                                                         //DMA_1__TD_TERMOUT_EN
-    CyDmaTdSetAddress(dmaTd[0], LO16((uint32)ADC_SAR_SAR_WRK0_PTR), LO16((uint32)ringBuffer));
+    CyDmaTdSetConfiguration(dmaTd[0], USB_PACKET_SIZE, dmaTd[0], TD_INC_DST_ADR | TD_AUTO_EXEC_NEXT | DMA_1__TD_TERMOUT_EN);
+    CyDmaTdSetAddress(dmaTd[0], LO16((uint32)ADC_SAR_Seq_SAR_SAR_WRK_PTR), LO16((uint32)usbPacketSignals));
     CyDmaChSetInitialTd(dmaChannel, dmaTd[0]);
-    CyDmaChEnable(dmaChannel, 1);
-}
-
-void GetLatestSamples(void){
-    uint16 readIndex;
-    if(writeIndex >= USB_PACKET_SIZE)
-        readIndex = writeIndex - USB_PACKET_SIZE;
-    else
-        readIndex = RING_SIZE + writeIndex - USB_PACKET_SIZE;
-
-    for(uint16 i = 0; i < USB_PACKET_SIZE; i++){
-        usbPacket[i] = ringBuffer[(readIndex + i) % RING_SIZE];
-    }
-}
-
-//===============================================
-//========== MAIN
-//===============================================
-int main(void){
-    CyGlobalIntEnable;
-    
-    DMA_Config();
-
-    USBUART_Start(0, USBUART_DWR_VDDD_OPERATION);
-    
-    AMux_1_Start();
-    AMux_1_Select(muxChannel);
-    ADC_SAR_Start();
-    ADC_SAR_IRQ_Disable();
     
     isrDMA_StartEx(DMA_ISR);
     
-    ADC_SAR_StartConvert();
+    CyDmaChEnable(dmaChannel, 1);
+}
+
+//===============================================
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// MAIN
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//===============================================
+int main(void){
+    CyGlobalIntEnable;
+
+    for(unsigned int i = 0; i < USB_PACKET_SIZE/2; i++){
+        usbPacketSignals[i] = (0x7E << 8) | 0x7C;
+    }
+
+    DMA_Config();
+
+    USBUART_Start(0, USBUART_DWR_VDDD_OPERATION);
+    //while(){};
+
+    ADC_SAR_Seq_Start();
+    ADC_SAR_Seq_IRQ_Disable();
+    
+    ADC_SAR_Seq_StartConvert();
     
     uint8 usbInitialized = 0;
 
@@ -97,8 +88,10 @@ int main(void){
                 usbInitialized = 1;
             }
             if(USBUART_CDCIsReady()){
-                GetLatestSamples();
-                USBUART_PutData(usbPacket, USB_PACKET_SIZE);
+                if(flagComplete){
+                    USBUART_PutData((uint8*)usbPacketSignals, USB_PACKET_SIZE);
+                    flagComplete = 0;
+                }
             }
         }else{
             usbInitialized = 0;
